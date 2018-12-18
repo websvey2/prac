169195c2485a6b2dc111f5f2e89b22edf16d7ddb
websvey2 <websvey@gmail.com>
2018-12-18 23:24:51 +0900

    prac1218

---
+++ silsoo.py
@@ -0,0 +1,11 @@
+# name = "홍길동"
+# print ("안녕하세요 {}입니다".format(name))
+
+# with open("silsu.txt", "r", encoding = 'utf-8') as f:
+#     data = f.read()
+#     print(data)
+
+with open("silsu.txt", 'w', encoding = 'utf-8') as f:
+    for i in range(1, 6):
+    	data = f"{i}번째 줄입니다.\n"
+    	f.write(data)
