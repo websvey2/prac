a743c0c9dc2e1b16f230d8a3a600c9db1801b761
websvey2 <websvey@gmail.com>
2018-12-18 23:37:36 +0900

    test

--- silsoo.py
+++ silsoo.py
@@ -5,7 +5,8 @@
 #     data = f.read()
 #     print(data)
 
-with open("silsu.txt", 'w', encoding = 'utf-8') as f:
-    for i in range(1, 6):
-    	data = f"{i}번째 줄입니다.\n"
-    	f.write(data)
+# with open("silsu.txt", 'w', encoding = 'utf-8') as f:
+#     for i in range(1, 6):
+#     	data = f"{i}번째 줄입니다.\n"
+#     	f.write(data)
+
